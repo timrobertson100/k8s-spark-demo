e6bf8e8c740d6a78fc57b16599000968b5a572ca
timrobertson100 <timrobertson100@gmail.com>
2025-10-02 16:53:40 +0200

    wip

--- src/main/java/org/gbif/demo/SparkDemo.java
+++ src/main/java/org/gbif/demo/SparkDemo.java
@@ -27,21 +27,66 @@ public class SparkDemo {
     SparkSession spark =
         SparkSession.builder()
             .appName("SparkDemo")
-            .config("spark.sql.warehouse.dir", "hdfs://gbif-hdfs" + targetDir + "/warehouse")
+            .config("spark.sql.warehouse.dir", "hdfs://gbif-hdfs" + targetDir + "/warehouse2")
             .getOrCreate();
 
-    setupWarehouse(spark, sourceDir);
+    // Difference is number of files. File size not all that significant at runtime
+    // setupWarehouse(spark, sourceDir); // copies and prepares data
+    // setupWarehouse2(spark, sourceDir); // copies and prepares data
+
+    // By reading from the parquet, it is SIGNIFICANTLY quicker than using the tables produced above
+    reloadWarehouse(spark, targetDir + "/warehouse2"); // loads the prepared data
+
+    spark
+        .sql(
+            """
+        SELECT
+          v.id,
+          v.verbatim as verbatim,
+          b.basic as basic,
+          t.temporal as temporal,
+          ta.taxonomy as taxonomy,
+          g.grscicoll as grscicoll,
+          l.location as location
+        FROM verbatim v
+          LEFT JOIN basic b ON v.id = b.id
+          LEFT JOIN temporal t ON v.id = t.id
+          LEFT JOIN taxonomy ta ON v.id = ta.id
+          LEFT JOIN grscicoll g ON v.id = g.id
+          LEFT JOIN location l ON v.id = l.id
+        """)
+        //        .coalesce(300)
+        .write()
+        .format("parquet")
+        .mode("overwrite")
+        .saveAsTable("output");
+  }
+
+  private static void reloadWarehouse(SparkSession spark, String sourceDir) {
+    spark.read().parquet(sourceDir + "/basic").createOrReplaceTempView("basic");
+    spark.read().parquet(sourceDir + "/grscicoll").createOrReplaceTempView("grscicoll");
+    spark.read().parquet(sourceDir + "/location").createOrReplaceTempView("location");
+    spark.read().parquet(sourceDir + "/taxonomy").createOrReplaceTempView("taxonomy");
+    spark.read().parquet(sourceDir + "/temporal").createOrReplaceTempView("temporal");
+    spark.read().parquet(sourceDir + "/verbatim").createOrReplaceTempView("verbatim");
   }
 
   private static void setupWarehouse(SparkSession spark, String sourceDir) {
-    rewriteToWarehouse(spark, sourceDir + "/extended_records", "verbatim", 250, false);
-    rewriteToWarehouse(spark, sourceDir + "/basic", "basic", 100, true);
-    rewriteToWarehouse(spark, sourceDir + "/grscicoll", "grscicoll", 100, true);
-    rewriteToWarehouse(spark, sourceDir + "/identifiers", "identifiers", 100, true);
-    rewriteToWarehouse(spark, sourceDir + "/location", "location", 100, true);
-    rewriteToWarehouse(spark, sourceDir + "/taxonomy", "taxonomy", 100, true);
-    rewriteToWarehouse(spark, sourceDir + "/temporal", "temporal", 100, true);
-    rewriteToWarehouse(spark, sourceDir + "/verbatim", "verbatim", 100, true);
+    rewriteToWarehouse(spark, sourceDir + "/basic", "basic", 100, true, false);
+    rewriteToWarehouse(spark, sourceDir + "/grscicoll", "grscicoll", 10, false, true);
+    rewriteToWarehouse(spark, sourceDir + "/location", "location", 100, true, false);
+    rewriteToWarehouse(spark, sourceDir + "/taxonomy", "taxonomy", 100, true, false);
+    rewriteToWarehouse(spark, sourceDir + "/temporal", "temporal", 50, true, false);
+    rewriteToWarehouse(spark, sourceDir + "/verbatim", "verbatim", 250, true, false);
+  }
+
+  private static void setupWarehouse2(SparkSession spark, String sourceDir) {
+    rewriteToWarehouse(spark, sourceDir + "/basic", "basic", 300, true, false);
+    rewriteToWarehouse(spark, sourceDir + "/grscicoll", "grscicoll", 300, false, true);
+    rewriteToWarehouse(spark, sourceDir + "/location", "location", 300, true, false);
+    rewriteToWarehouse(spark, sourceDir + "/taxonomy", "taxonomy", 300, true, false);
+    rewriteToWarehouse(spark, sourceDir + "/temporal", "temporal", 300, true, false);
+    rewriteToWarehouse(spark, sourceDir + "/verbatim", "verbatim", 300, true, false);
   }
 
   // Reads, repartition and replace the table in warehouse
@@ -50,25 +95,27 @@ public class SparkDemo {
       String sourceDir,
       String tableName,
       int partitionCount,
-      boolean expandToKVP) {
+      boolean expandToKVP,
+      boolean renameToKVP) {
     spark.sql(String.format("DROP TABLE IF EXISTS %s", tableName));
 
-    Dataset<Row> input =
-        expandToKVP
-            ? spark
-                .read()
-                .parquet(sourceDir)
-                .as(Encoders.tuple(Encoders.STRING(), Encoders.STRING()))
-                .toDF("id", tableName)
-            : spark.read().parquet(sourceDir);
+    Dataset<Row> input;
+
+    if (expandToKVP)
+      input =
+          spark
+              .read()
+              .parquet(sourceDir)
+              .as(Encoders.tuple(Encoders.STRING(), Encoders.STRING()))
+              .toDF("id", tableName);
+    else if (renameToKVP) input = spark.read().parquet(sourceDir).toDF("id", tableName);
+    else input = spark.read().parquet(sourceDir);
 
     input
         .repartition(partitionCount)
         .write()
         .format("parquet")
         .mode("overwrite")
-        // .bucketBy(10, "id")
-        // .sortBy("id")
         .saveAsTable(tableName);
   }
 }
